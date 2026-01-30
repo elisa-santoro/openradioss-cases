ce92e02019bc12f5f4c96c359845a0e66186c9ca
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-01-30 14:39:29 +0100

    Let toVTK and toCSV continue in case of errors (#9)

--- tools/setup-openradioss.sh
+++ tools/setup-openradioss.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e -u
 shopt -s expand_aliases
 
 # Set OPENRADIOSS_PATH in your ~/.bashrc, or uncomment the next line
@@ -8,24 +7,23 @@ export RAD_CFG_PATH=$OPENRADIOSS_PATH/hm_cfg_files
 export OMP_STACKSIZE=400m
 export LD_LIBRARY_PATH="$OPENRADIOSS_PATH/extlib/hm_reader/linux64/:$OPENRADIOSS_PATH/extlib/h3d/lib/linux64/:$LD_LIBRARY_PATH"
 
-alias radioss_starter="$OPENRADIOSS_PATH/starter/cbuild_starter_linux64_gf/starter_linux64_gf"
-alias radioss_engine="$OPENRADIOSS_PATH/engine/cbuild_engine_linux64_gf_ompi_precice/engine_linux64_gf_ompi_precice"
-alias radiossVTK="$OPENRADIOSS_PATH/exec/anim_to_vtk_linux64_gf"
-alias radiossCSV="$OPENRADIOSS_PATH/exec/th_to_csv_linux64_gf"
+alias radioss_starter='$OPENRADIOSS_PATH/starter/cbuild_starter_linux64_gf/starter_linux64_gf'
+alias radioss_engine='$OPENRADIOSS_PATH/engine/cbuild_engine_linux64_gf_ompi_precice/engine_linux64_gf_ompi_precice'
+alias radiossVTK='$OPENRADIOSS_PATH/exec/anim_to_vtk_linux64_gf'
+alias radiossCSV='$OPENRADIOSS_PATH/exec/th_to_csv_linux64_gf'
 
 # Run `toVTK modelA` to convert all animation files modelAA* to VTK format
 toVTK() {
 
     if [ $# -lt 1 ]; then
-        echo "Usage: toVTK ROOTNAME"
-        return 1
+        echo "ERROR - Usage: toVTK ROOTNAME"
     fi
 
     Rootname="$1"
 
     for file in "${Rootname}"A*; do
         # skip if no matching files
-        [ -e "$file" ] || { echo "No files matching ${Rootname}A*"; return 1; }
+        [ -e "$file" ] || { echo "ERROR: No files matching ${Rootname}A*"; }
 
         animation_number="${file#"${Rootname}A"}"
 
@@ -35,19 +33,18 @@ toVTK() {
     done
 }
 
-# Run `toCSV modelA` to convert all animation files modelAA* to CSV format
+# Run `toCSV modelA` to convert all animation files modelAT* to CSV format
 toCSV() {
 
     if [ $# -lt 1 ]; then
-        echo "Usage: toVTK ROOTNAME"
-        return 1
+        echo "ERROR - Usage: toVTK ROOTNAME"
     fi
 
     Rootname="$1"
 
     for file in "${Rootname}"T*; do
         # skip if no matching files
-        [ -e "$file" ] || { echo "No files matching ${Rootname}T*"; return 1; }
+        [ -e "$file" ] || { echo "ERROR: No files matching ${Rootname}T*"; }
 
         history_number="${file#"${Rootname}T"}"
 
